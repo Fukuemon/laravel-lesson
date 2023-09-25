c90496ad4a8897e7c2c32142d5e366c4d71bb041
Fukuemon <Fukuemony029@gmail.com>
2023-09-25 13:51:08 +0900

    ✨ テーブルからデータを取得

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('tests.test');
+        $values = Test::all(); // 全て取得
+
+        dd($values); // 値を確認したい時に利用する(dieとvar_dumpの合わせ技)
+
+        return view(tests.test, compact("values")); // compact関数でviewに渡す
     }
 }
