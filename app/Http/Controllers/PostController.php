da46bac4546f57daa510758f5818b047441cac88
Fukuemon <Fukuemony029@gmail.com>
2023-09-25 15:43:10 +0900

    ✨ コントローラーの作成 #2
    全てのPostデータを取得し、postViewに反映した状態でviewを返す

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $values = Pest::all(); // 全て取得
+
+        // dd($values); // 値を確認したい時に利用する(dieとvar_dumpの合わせ技)
+
+        return view("posts.post", compact("values")); // compact関数でviewに渡す
+    }
 }
