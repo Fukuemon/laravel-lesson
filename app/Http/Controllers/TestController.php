9118d953c3db05a4fe6f4d65cffe12761466e180
Fukuemon <Fukuemony029@gmail.com>
2023-10-16 13:27:57 +0900

    ✨ エロクアントでいろんなコレクション型を取得 #4

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,9 +9,13 @@ class TestController extends Controller
 {
     public function index()
     {
+        // Eloquant ORM
         $values = Test::all(); // 全て取得
+        $count = Test::count(); // レコード数を取得
+        $first = Test::findOrFail(1); // idが1のレコードを取得
+        $whereBBB = Test::where('text', '=', 'BBB')->get(); // レコードの中からtextがBBBのレコードを取得
 
-        // dd($values); // 値を確認したい時に利用する(dieとvar_dumpの合わせ技)
+        dd($values, $count, $first , $whereBBB); // 値を確認したい時に利用する(dieとvar_dumpの合わせ技)
 
         return view("tests.test", compact("values")); // compact関数でviewに渡す
     }
