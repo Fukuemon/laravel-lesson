e497cfb2e0271367f324ca4e0ed2d8f12033d373
Fukuemon <Fukuemony029@gmail.com>
2023-10-23 14:27:06 +0900

    🗃️ お問い合わせフォームのテーブル構造を作成 #8

--- database/migrations/2023_10_23_141403_create_contact_forms_table.php
+++ database/migrations/2023_10_23_141403_create_contact_forms_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('contact_forms', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 20); //名前
+            $table->string('email', 255); // メールアドレス
+            $table->longText('url')->nullable(); // メッセージ
+            $table->boolean('gender'); //性別
+            $table->tinyInteger('age'); //年齢
+            $table->string('contact', 200); //お問い合わせ種類
             $table->timestamps();
         });
     }
